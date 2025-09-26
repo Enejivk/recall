2350a184a6f8f135a14333fb7b158a9b2ccb2212
Eneji Victor <enejivic@gmail.com>
2025-09-26 20:24:29 +0100

    done with auth exept for forget password

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,6 +1,38 @@
-import type { Request, Response } from "express";
+import type { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
+dotenv.config();
+declare global {
+  namespace Express {
+    interface Request {
+      user?: string | jwt.JwtPayload;
+    }
+  }
+}
 
-function AuthMiddleware(req: Request, res:Response){
-    req.complete
+export function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const accessToken = req.cookies.accessToken;
+  const secretKey = process.env.JWT_SECRET;
+
+  if (!secretKey) {
+    return res.status(500).json({ message: "No secret key found" });
+  }
+
+  if (!accessToken) {
+    return res.status(401).json({ message: "Access token missing" });
+  }
+
+  try {
+    const payload = jwt.verify(accessToken, secretKey);
+    req.user = payload;
+    console.log(req.user)
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired access token" });
+  }
 }
